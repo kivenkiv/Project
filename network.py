4f3f2ea30449f6c6e4efe3d15fdc0627c9053a90
kivenkiv <48606097+kivenkiv@users.noreply.github.com>
2019-04-11 13:17:52 -0400

    Update network.py

--- network.py
+++ network.py
@@ -12,13 +12,18 @@ class net(nn.Module):
     def __init__(self):
         """Load the pretrained ResNet-152 and replace top fc layer."""
         super(net, self).__init__()
-        self.resnet = models.resnet152(pretrained=True)
+        resnet = models.resnet152(pretrained=True)
+        modules = list(resnet.children())[:-1]      # delete the last fc layer.
+        self.resnet = nn.Sequential(*modules)
+        self.linear = nn.Linear(resnet.fc.in_features, 44)
+        self.bn = nn.BatchNorm1d(44, momentum=0.01)
         
     def forward(self, images):
         """Extract feature vectors from input images."""
         with torch.no_grad():
             features = self.resnet(images)
-            print(features.shape)
+        features = features.reshape(features.size(0), -1)
+        features = self.bn(self.linear(features))
         return features
 
 
