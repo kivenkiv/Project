969471222e9e3e2b2e55f678044386e8326d6a47
kivenkiv <48606097+kivenkiv@users.noreply.github.com>
2019-04-11 13:57:14 -0400

    Update network.py

--- network.py
+++ network.py
@@ -26,7 +26,7 @@ class net(nn.Module):
         features = self.bn(self.linear(features))
         print(features.shape)
         print(features.type)
-        feature.type(torch.Scalar)
+        features.type(torch.Scalar)
         return features
 
 
